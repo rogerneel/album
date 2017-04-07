cd858745ed9514bcac73f31f4dc612d6ff19aeeb
Roger Neel <roger@mavenlink.com>
2017-04-07 14:41:28 -0700

    the most basic, working ios app

---
+++ index.ios.js
@@ -0,0 +1,15 @@
+// Index.ios.js place code in here for iOS
+
+// Import a library to help create a Component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Create a Component
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
