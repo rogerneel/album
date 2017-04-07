b2718b6010ec2243d49dda839f7f9bb75fafca28
Roger Neel <roger@mavenlink.com>
2017-04-07 14:56:46 -0700

    making header and using it

--- index.ios.js
+++ index.ios.js
@@ -2,12 +2,13 @@
 
 // Import a library to help create a Component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create a Component
 const App = () => {
   return (
-    <Text>Some Text</Text>
+    <Header />
   );
 };
 
